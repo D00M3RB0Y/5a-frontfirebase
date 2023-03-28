9643dba6ee29749aecc5453642cd53ce7072ada1
D00M3RB0Y <122471874+D00M3RB0Y@users.noreply.github.com>
2023-03-28 11:59:29 -0600

    Inicio

---
+++ app.js
@@ -0,0 +1,9 @@
+//Cuadros de dialogo
+const openModal = document.getElementById('openRegisterModal')
+const modal = document.getElementById('modal')
+
+const showRegisterModal = () => {
+    modal.classList.toggle('is-active')
+}
+
+openModal.addEventListener('click', showRegisterModal)
