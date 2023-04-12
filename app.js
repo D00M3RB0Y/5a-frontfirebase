80d682e2223e56e38d665dfb5a1c3e3598061186
D00M3RB0Y <122471874+D00M3RB0Y@users.noreply.github.com>
2023-04-12 12:18:38 -0600

    90% de terminado

--- app.js
+++ app.js
@@ -1,63 +1,71 @@
-const btnBorrar = document.getElementById('btnBorrar')
+const btnBorrar = document.getElementById("btnBorrar");
 
-document.addEventListener('DOMContentLoaded', () => {
-    cargaContactos()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  cargaContactos();
+});
 
-
-agenda.addEventListener('click', (e) => {
-    
-    if(e.target.classList.contains('btn-warning') || e.target.classList.contains('fa-trash')){
-        console.log(e.target.dataset.id)
-        fetch('http://localhost:20000/delete', {
-            method: 'POST',
-            body: JSON.stringify({
-                email:e.target.dataset.id
-            }),
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-            }
-        })
-            .then( async (res) => {
-                console.log('delete', await res.json())
-            })
-            .catch((error) => {
-                console.log('error', error)
-            })
-    }
-})
-
-btnBorrar.addEventListener('click', () => {
-    const email = document.getElementById('email').value
-
-    fetch('http://localhost:20000/delete', {
-        method: 'POST',
-        body: JSON.stringify({
-            email
-        }),
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-        }
+agenda.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("btn-warning") ||
+    e.target.classList.contains("fa-trash")
+  ) {
+    console.log(e.target.dataset.id);
+    fetch("http://localhost:20000/delete", {
+      method: "POST",
+      body: JSON.stringify({
+        email: e.target.dataset.id,
+      }),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
     })
-        .then( async (res) => {
-            console.log('delete', await res.json())
-        })
-        .catch((error) => {
-            console.log('error', error)
-        })
-})
+      .then(async (res) => {
+        console.log("delete", await res.json());
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }
+  if (
+    e.target.classList.contains("btn-danger") ||
+    e.target.classList.contains("fa-pencil-alt")
+  ) {
+    var inputNombre = document.getElementById("email3");
+    inputNombre.value = e.target.dataset.id;
+  }
+});
+
+btnBorrar.addEventListener("click", () => {
+  const email = document.getElementById("email").value;
+
+  fetch("http://localhost:20000/delete", {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then(async (res) => {
+      console.log("delete", await res.json());
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+});
 
 const cargaContactos = () => {
-    const options = {
-        method: 'GET'
-    }
-    fetch('http://localhost:20000/read', options)
-        .then(async (res) => {
-            const contactos = await res.json()
-            console.log('contactos', contactos.regresa)
-            contactos.regresa.forEach((contacto) => {
-                const contactoInfo = contacto
-                agenda.innerHTML += `<tr>
+  const options = {
+    method: "GET",
+  };
+  fetch("http://localhost:20000/read", options)
+    .then(async (res) => {
+      const contactos = await res.json();
+      console.log("contactos", contactos.regresa);
+      contactos.regresa.forEach((contacto) => {
+        const contactoInfo = contacto;
+        agenda.innerHTML += `<tr>
                 <td>${contactoInfo.nombre}</td>
                 <td>${contactoInfo.apaterno}</td>
                 <td>${contactoInfo.amaterno}</td>
@@ -67,54 +75,87 @@ const cargaContactos = () => {
                 <td>${contactoInfo.estado}</td>
                 <td>${contactoInfo.email}</td>
                 <td>
-                    <button type="submit" class="btn btn-danger" data-id="${contactoInfo.email}">
-                        <i class="fas fa-pencil-alt"></i>
+                    <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal2" data-id="${contactoInfo.email}">
+                        <i class="fas fa-pencil-alt" data-bs-toggle="modal" data-bs-target="#exampleModal2" data-id="${contactoInfo.email}"></i>
                     </button>
                     <button id="showDialog" class="btn btn-warning marco" data-id="${contactoInfo.email}">
                         <i class="fas fa-trash" data-id="${contactoInfo.email}"></i>
                     </button>
                 </td>
-                </tr>`
-            });
-        })
-        .catch((error) => {
-            console.log('error' , error)
-        })
-}
+                </tr>`;
+      });
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+};
 
 const insertaContacto = () => {
-    const nombre = document.getElementById('nombre')
-    const apaterno = document.getElementById('apaterno')
-    const amaterno = document.getElementById('amaterno')
-    const direccion = document.getElementById('direccion')
-    const telefono = document.getElementById('telefono')
-    const ciudad = document.getElementById('ciudad')
-    const estado = document.getElementById('estado')
-    const email = document.getElementById('email')
+  const nombre = document.getElementById("nombre");
+  const apaterno = document.getElementById("apaterno");
+  const amaterno = document.getElementById("amaterno");
+  const direccion = document.getElementById("direccion");
+  const telefono = document.getElementById("telefono");
+  const ciudad = document.getElementById("ciudad");
+  const estado = document.getElementById("estado");
+  const email = document.getElementById("email2");
 
+  console.log(email.value);
 
-
-
-    fetch('http://localhost:20000/create', {
-        method: 'POST',
-        body: JSON.stringify({
-            nombre: nombre.value,
-            apaterno: apaterno.value,
-            amaterno: amaterno.value,
-            direccion: direccion.value,
-            telefono: telefono.value,
-            ciudad: ciudad.value,
-            estado: estado.value,
-            email: email.value
-        }),
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-        }
+  fetch("http://localhost:20000/create", {
+    method: "POST",
+    body: JSON.stringify({
+      nombre: nombre.value,
+      apaterno: apaterno.value,
+      amaterno: amaterno.value,
+      direccion: direccion.value,
+      telefono: telefono.value,
+      ciudad: ciudad.value,
+      estado: estado.value,
+      email: email.value,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then(async (res) => {
+      console.log("create", await res.json());
     })
-        .then( async (res) => {
-            console.log('create', await res.json())
-        })
-        .catch((error) => {
-            console.log('error', error)
-        })
-}
+    .catch((error) => {
+      console.log("error", error);
+    });
+};
+
+const actualizaContacto = () => {
+  const nombre = document.getElementById("nombre2");
+  const apaterno = document.getElementById("apaterno2");
+  const amaterno = document.getElementById("amaterno2");
+  const direccion = document.getElementById("direccion2");
+  const telefono = document.getElementById("telefono2");
+  const ciudad = document.getElementById("ciudad2");
+  const estado = document.getElementById("estado2");
+  const email = document.getElementById("email3");
+
+  fetch("http://localhost:20000/update", {
+    method: "POST",
+    body: JSON.stringify({
+      nombre: nombre.value,
+      apaterno: apaterno.value,
+      amaterno: amaterno.value,
+      direccion: direccion.value,
+      telefono: telefono.value,
+      ciudad: ciudad.value,
+      estado: estado.value,
+      email: email.value,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then(async (res) => {
+      console.log("update", await res.json());
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+};
